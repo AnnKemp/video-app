24c6d636beb9950474e1863f84dc2d0e1c5eda52
AnnKemp <ann.kemp@scarlet.be>
2020-04-16 19:03:47 +0200

    studying hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function Example() {
   // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0);
+  const [age, setAge] = useState(42);
+  const [fruit, setFruit] = useState('banana');
+  const [todos, setTodos] = useState([{ text: 'Learn Hooks' }]);
+  // je mag meerdere hooks gebruiken in een enkel component
+
+  function Example(props) { // kan ik die in de functie gebruiken? Nog even checken . . .
+    // You can use Hooks here!
+    return <div />;
+  }
+  const Examplee = (props) => { // exact hetzelfde als de functie hierboven, mag ik die hier gebruiken? Nog even checken . . .
+    // You can use Hooks here!
+    return <div />;
+  }
 
   return (
       <div>

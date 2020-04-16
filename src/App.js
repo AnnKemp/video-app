ed851ba70baa9164fa7efb0d5d84b0cbe28b256f
AnnKemp <ann.kemp@scarlet.be>
2020-04-16 20:42:22 +0200

    cleaned up some code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-
 function App() {
   const [count, setCount] = useState(0);
 
-  useEffect(() => { // is gelijk aan componentDidMount() en componentDidUpdate()
+  useEffect(() => { // is gelijk aan componentDidMount() én componentDidUpdate()
     // perform data fetching or call some other imperative API, hier moet ik dus mijn data gaan fetchen
     document.title = `You clicked ${count} times`;
+    return function cleanup() { // is gelijk aan: componentWillUnmount() bij OOP
+      // hier komt de cleanup code
+    };
   });
 
   return (

7fe6c3efba7407e48e5de68baa6e43a808c36ce5
AnnKemp <ann.kemp@scarlet.be>
2020-04-20 21:42:43 +0200

    adding map with try-out of the page in javascript and html and CSs

--- src/App.js
+++ src/App.js
@@ -20,10 +20,7 @@ function App() {
     );
   return (
       <div>
-        <p>You clicked {count} times</p>
-        <button onClick={() => setCount(count + 1)}>
-          Click me
-        </button>
+          <div id="box"></div>
       </div>
   );
 }

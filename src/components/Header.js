a53b6394be19709302bc4669c2c54e0434c64107
AnnKemp <ann.kemp@scarlet.be>
2020-04-27 00:06:44 +0200

    raw version of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,12 +17,12 @@ import React from "react";
 function Header() {
     return (
     <header>
-        <h1 id="title"></h1>
+        <h1 id="title">Here comes the title</h1>
         <video controls>
             <source type="video/mp4" />
                 Your browser does not support the video tag.
         </video>
-        <p></p>
+        <p>And here the description</p>
     </header>
     );
 }

d1e731abad275ec116c5b821cef21773ca683553
AnnKemp <ann.kemp@scarlet.be>
2020-05-02 23:30:29 +0200

    update all

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import CardView from "./CardView";
 
 const Card = () => {
     const [data, setData] = useState({results: []});
@@ -11,29 +12,12 @@ const Card = () => {
             console.log(data.results); // test the outcome
         })();
     }, []);
-
     //return data;
-    return(
-        data.results.map(item => { // this way I can map all the needed elements instead of one (with two returns nested)
-            return (
-                <figure>
-                    <h1 key={item.id}>{item.original_title}</h1>
-                    <img src={"https://image.tmdb.org/t/p/original"+item.poster_path} alt={item.original_title} />
-                    <em>&#10031; {item.vote_average}</em>
-                    <details><summary>Movie description</summary>
-                        {item.overview}
-                    </details>
-                </figure>
-            );
-        })
+    return( // nog eens even checken hoe ik de data van de state hier het beste mee geef als 'props'
+        <CardView data={data} />
     );
 }
 export default Card;
-
-
-
-
-
 
 /*
 
--- src/components/upcoming/Upcoming.js
+++ src/components/upcoming/Upcoming.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import CardView from "../CardView";
 
 const Upcoming = () =>{
-// dit nog een beetje aanpassen
+
         const [data, setData] = useState({results: []});
 
         useEffect(() => {
@@ -9,24 +10,11 @@ const Upcoming = () =>{
                 const res = await fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=7e79f7263414fc6a1544c1a5e219faa6");
                 const data = await res.json();
                 setData(data);
-                console.log(data.results); // test the outcome
+                //console.log(data.results); // test the outcome
             })();
         }, []);
-
-        //return data;
         return(
-            data.results.map(item => { // this way I can map all the needed elements instead of one (with two returns nested)
-                return (
-                    <figure>
-                        <h1 key={item.id}>{item.original_title}</h1>
-                        <img src={"https://image.tmdb.org/t/p/original"+item.poster_path} alt={item.original_title} />
-                        <em>&#10031; {item.vote_average}</em>
-                        <details><summary>Movie description</summary>
-                            {item.overview}
-                        </details>
-                    </figure>
-                );
-            })
+            <main id="box" style={{marginTop:"55px"}}><CardView data={data} /></main>
     );
 }
 export default Upcoming;

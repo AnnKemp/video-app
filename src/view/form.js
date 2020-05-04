1e672c6dfee26996607763150b15bfa6537eeb9b
AnnKemp <ann.kemp@scarlet.be>
2020-05-04 15:18:47 +0200

    updating

--- src/view/form.js
+++ src/view/form.js
@@ -1,35 +1,34 @@
 import React, { useState } from 'react';
 
 const MyForm= () => {
-    const [movie, setMovie] = useState(""); // hier komt de waarde die in het invulveld wordt ingevuld
-    const [data, setData] = useState({results: []}); // hier komen de data opgevraagd van de api
+    const [movie, setMovie] = useState(""); // here comes the value from the input-field
+    const [data, setData] = useState({results: []}); // here comes the data from the api-search
 
-    function myChangeHandler(event) {
+    let film=""; // initiating let film
+    let foundMovies;
+
+    function myChangeHandler(event) { // if type something in the input-field
         setMovie(event.target.value);
     }
-    let film="";
 
-    function mySubmitHandler(event){
-      event.preventDefault();
+    function mySubmitHandler(event){ // if you click on 'search'
+      event.preventDefault(); // normally with a submit-button but ... anyway . . .
       film=movie;
       Search();
     }
    // "https://api.themoviedb.org/3/search/movie/?api_key=67b347978ffe14fc5d6f8a664a1829f2&query=" // dit is de juiste query
 
     const Search=() => {
-            alert(film);
             if(movie!=="") {
             const url = "https://api.themoviedb.org/3/search/movie/?api_key=67b347978ffe14fc5d6f8a664a1829f2&query=";
             let total = url+film;
-
-             let foundMovies;
 
                 fetch(total)
                     .then(response => response.json())
                     .then(data => {
                         console.log('Success:', data);
                         setData(data);
-                        return foundMovies=data; // ok data gevonden en er uit gehaald nu nog ze kunnen terugsturen om ze te tonen
+                        return foundMovies=data; // ok data gevonden en er uit gehaald nu nog ze kunnen terugsturen om ze te tonen op een andere pagina
                     })
             }else{}
     }

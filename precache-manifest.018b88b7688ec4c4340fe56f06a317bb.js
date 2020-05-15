92b0d37428ffd5065b67a284fc3da8686956dd7e
AnnKemp <ann.kemp@scarlet.be>
2020-05-15 16:27:49 +0200

    Updates

---
+++ precache-manifest.018b88b7688ec4c4340fe56f06a317bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee65f334399c018597d4de82bd979165",
+    "url": "/video-app/index.html"
+  },
+  {
+    "revision": "450007cc38c2f88e5885",
+    "url": "/video-app/static/css/main.685562cb.chunk.css"
+  },
+  {
+    "revision": "c69050654fbb1ec4f3dc",
+    "url": "/video-app/static/js/2.be9ac1a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/video-app/static/js/2.be9ac1a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "450007cc38c2f88e5885",
+    "url": "/video-app/static/js/main.c040f0cb.chunk.js"
+  },
+  {
+    "revision": "b94e468e443c4e8e0769",
+    "url": "/video-app/static/js/runtime-main.5dcf1742.js"
+  }
+]);

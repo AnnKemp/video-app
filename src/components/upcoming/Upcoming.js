441cd9bad739534ac4684da0bf5b06de19dc8a8c
AnnKemp <ann.kemp@scarlet.be>
2020-05-15 18:40:02 +0200

    voorlogige stand van de CSS, 'k ga nu eten

--- src/components/upcoming/Upcoming.js
+++ src/components/upcoming/Upcoming.js
@@ -1,20 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import CardView from "../CardView";
+import Footer from "../Footer";
 
 const Upcoming = () =>{
-
         const [data, setData] = useState({results: []});
-
         useEffect(() => {
             (async () => {
                 const res = await fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=7e79f7263414fc6a1544c1a5e219faa6");
                 const data = await res.json();
                 setData(data);
-                //console.log(data.results); // test the outcome
             })();
         }, []);
         return(
+            <div>
             <main id="box" style={{marginTop:"55px"}}><CardView data={data} /></main>
+            <Footer />
+            </div>
     );
 }
 export default Upcoming;
